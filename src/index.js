ce18a0efe215d0adde159112a8c1a993564540c3
Anand Angalig <anandangalig@gmail.com>
2019-03-06 16:31:25 -0500

    html VS. JSX

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return {text: 'Clack'};
+}
 // Create a react component: can be function based OR class based
 const App = () => {
     return (
         <div>
             <h1>I want donuts!</h1>
+            <label className="label" htmlFor="name">Enter donut name:</label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{getButtonText().text}</button> 
+            {/* {{}}: first is for JSX expression/interpolation, second is for the object containing the style */}
         </div>
     );
 }
@@ -14,4 +21,4 @@ const App = () => {
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
-);
+);                                                                                                                                                 
