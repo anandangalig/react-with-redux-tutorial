cd7356e0c86903395c57d171ca5ca0309f1a7b82
Anand Angalig <anandangalig@gmail.com>
2019-03-07 16:37:10 -0500

    refactored App component to be class based, and use of setState

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    const geolocation = window.navigator.geolocation.getCurrentPosition(
-        (pos)=>console.log(pos),
-        (err)=>console.log(err)
+class App extends React.Component {
+    constructor(props) {
+        super(props); // calls the constructor function of the parent class
+        this.state = {lat: null, errorMessage: ''}; // the ONLY time we directly set state
+         
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+    render() {
+        if(this.state.lat && !this.state.errorMessage) {
+            return (<div>Latitude: {this.state.lat}</div>);
+        }
+        if(!this.state.lat && this.state.errorMessage) {
+           return (<div>Error: {this.state.errorMessage}</div>);
+        }
+        return (<div>Loading...</div>);
         
-    );
-    return (
-        <div>test</div>
-    );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
