d351c2c42d38fe4b533af8153e6641ed73631445
Anand Angalig <anandangalig@gmail.com>
2019-03-13 17:01:10 -0400

    hello REF system! tweaked grid spacing

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props){
+        super(props);
+        this.imageRef = React.createRef();
+        this.state = {rowSpans: null}
+    }
+
+    componentDidMount() {
+        // adding vanilla js event listener on load to make sure span calc is done after images are fetched
+        this.imageRef.current.addEventListener('load', this.setRowSpan);
+    }
+    
+    setRowSpan = () => {
+        this.setState({rowSpans: Math.ceil(this.imageRef.current.height / 10)});
+    }
+
     render() {
         const {description, urls} = this.props.image;        
         return(
-            <img alt={description} src={urls.regular} />
+            <img style={{gridRowEnd: `span ${this.state.rowSpans}`}} ref={this.imageRef} alt={description} src={urls.regular} />
         );
     }
 }
