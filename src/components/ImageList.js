b92a98e478bc883320f4a10a13d948a506a350ee
Anand Angalig <anandangalig@gmail.com>
2019-03-13 16:06:30 -0400

    created image card component to handle individual images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,15 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {   
     const images = props.images.map(
-        ({id, description, urls}) => {
-            return <img key={id} alt={description} src={urls.small} />;
+        (image) => {
+            return <ImageCard key={image.id} image={image} />;
         }   
     );
 
-    return <div>{images}</div>;
+    return <div className='image-list'>{images}</div>;
 }
 
 export default ImageList;
